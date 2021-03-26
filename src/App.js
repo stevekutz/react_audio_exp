e2f1f61ed1083534378601617be923b1a30e4dcf
stevekutz <stkutz@gmail.com>
2021-03-26 12:53:56 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  // use Audio constructor to create HTMLAudioElement
+  const audioTune = new Audio('./assets/sounds/longbeep.mp3');
+
+  // variable to play audio in loop
+  const [playInLoop, setPlayInLoop] = useState(false);
+
+  // load audio file on component load
+  useEffect(() => {
+    audioTune.load();
+  }, [])
+
+  // set the loop of audio tune
+  useEffect(() => {
+    audioTune.loop = playInLoop;
+  }, [playInLoop])
+
+  // play audio sound
+  const playSound = () => {
+    audioTune.play();
+  }
+
+  // pause audio sound
+  const pauseSound = () => {
+    audioTune.pause();
+  }
+
+  // stop audio sound
+  const stopSound = () => {
+    audioTune.pause();
+    audioTune.currentTime = 0;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <input type="button" className="btn btn-primary mr-2" value="Play" onClick={playSound}></input>
+      <input type="button" className="btn btn-warning mr-2" value="Pause" onClick={pauseSound}></input>
+      <input type="button" className="btn btn-danger mr-2" value="Stop" onClick={stopSound}></input>
+
+      <label><input type="checkbox" checked={playInLoop} onChange={e => setPlayInLoop(e.target.checked)} /> Play in Loop</label>
+      <div className="mt-5"><b>Note: </b> In order to make it work, You have to update the path of the audio file in App.js file.</div>
     </div>
   );
 }
