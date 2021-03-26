a79273790396ce8531594f28740f30fd79ab39c3
stevekutz <stkutz@gmail.com>
2021-03-26 16:20:06 -0500

    cleaned up useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import React, { useEffect, useState } from 'react';
+import longBeep from './assets/sounds/longbeep.mp3';
+import click from './click.mp3'
 
 function App() {
 
   // use Audio constructor to create HTMLAudioElement
-  const audioTune = new Audio('./assets/sounds/longbeep.mp3');
+// const audioTune = new Audio(click);    //  click  plays sound
+
+const audioTune = new Audio(click); //    'click' does not play sound
+  console.log("audioTune obj >> ", audioTune);
 
   // variable to play audio in loop
   const [playInLoop, setPlayInLoop] = useState(false);
@@ -12,12 +17,13 @@ function App() {
   // load audio file on component load
   useEffect(() => {
     audioTune.load();
-  }, [audioTune])
+  })
 
   // set the loop of audio tune
+    // uses Boolean attribute
   useEffect(() => {
     audioTune.loop = playInLoop;
-  }, [playInLoop, audioTune])
+  });
 
   // play audio sound
   const playSound = () => {
@@ -38,13 +44,16 @@ function App() {
   return (
     <div className="App">
       
+      <div style = {{border: "1px solid green"}} >
+        <h1> Using Audio constructor inherited from HTMLAudioElement</h1>
+        <input type="button" className="btn btn-primary mr-2" value="Play" onClick={playSound}></input>
+        <input type="button" className="btn btn-warning mr-2" value="Pause" onClick={pauseSound}></input>
+        <input type="button" className="btn btn-danger mr-2" value="Stop" onClick={stopSound}></input>
 
-      <input type="button" className="btn btn-primary mr-2" value="Play" onClick={playSound}></input>
-      <input type="button" className="btn btn-warning mr-2" value="Pause" onClick={pauseSound}></input>
-      <input type="button" className="btn btn-danger mr-2" value="Stop" onClick={stopSound}></input>
+        <label><input type="checkbox" checked={playInLoop} onChange={e => setPlayInLoop(e.target.checked)} /> Play in Loop</label>
+      </div>
 
-      <label><input type="checkbox" checked={playInLoop} onChange={e => setPlayInLoop(e.target.checked)} /> Play in Loop</label>
-      <div className="mt-5"><b>Note: </b> In order to make it work, You have to update the path of the audio file in App.js file.</div>
+      
     </div>
   );
 }
